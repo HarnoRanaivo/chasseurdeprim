51e2c1aa54041be206f48e30d43de7bfb1f59ed8
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2013-03-21 23:37:10 +0100

    suite fonctions graphes

--- src/graphe.c
+++ src/graphe.c
@@ -10,10 +10,67 @@ Graphe gNouv(void)
     return NULL;
 }
 
-Graphe gAjoutSommet(Graphe g, Sommet s);
-Graphe gAjoutArete(Graphe g, Sommet a, Sommet b, Ent p);
+Graphe gAjoutSommet(Graphe g, Sommet s)
+{
+    if (!gExisteSommet(g, s))
+    {
+        Graphe g0 = NULL;
+        g0 = MALLOC(g0);
+        if (g0 != NULL)
+        {
+            g0->sommet = s;
+            g0->listeadjacence = listnouv();
+            g0->suivant = g;
+            g = g0;
+        }
+    }
+
+    return g;
+}
+
+Graphe gAjoutArete(Graphe g, Sommet a, Sommet b, Ent p)
+{
+    if (!gExisteArete(g, a, b))
+    {
+        Graphe ga, gb;
+
+        if (!gExisteSommet(g, a) && gExisteSommet(g, b))
+            g = gAjoutSommet(g, a);
+        else if (!gExisteSommet(g, b) && gExisteSommet(g, b))
+            g = gAjoutSommet(g, b);
+
+        ga = gPSommet(g, a);
+        ga->listeadjacence = lajar(ga->listeadjacence, b, p);
+        gb = gPSommet(g, b);
+        gb->listeadjacence = lajar(gb->listeadjacence, a, p);
+    }
+
+    return g;
+}
+
+static Graphe gPrecedent(Graphe g, Sommet s)
+{
+    if (gEstVide(g)) return NULL;
+    else if (gEstVide(g->suivant)) return NULL;
+    else if (egalSom(g->suivant->sommet, s)) return g;
+    else return gPrecedent(g->suivant, s);
+}
+
+/* TODO. */
 Graphe gSupprimerSommet(Graphe g, Sommet s);
-Graphe gSupprimerArete(Graphe g, Sommet a, Sommet b);
+
+Graphe gSupprimerArete(Graphe g, Sommet a, Sommet b)
+{
+    Graphe ga = gPSommet(g, a);
+    Graphe gb = gPSommet(g, b);
+
+    ga->listeadjacence = lsupar(ga->listeadjacence, b);
+    gb->listeadjacence = lsupar(gb->listeadjacence, a);
+
+    return g;
+}
+
+/* TODO. */
 Graphe gModifierArete(Graphe g, Sommet a, Sommet b, Ent p);
 
 Bool gEstVide(Graphe g)
@@ -23,7 +80,7 @@ Bool gEstVide(Graphe g)
 
 Bool gExisteSommet(Graphe g, Sommet s)
 {
-    if (gEstVide(g)) return NULL;
+    if (gEstVide(g)) return FAUX;
     else if (egalSom(g->sommet, s)) return VRAI;
     else return gExisteSommet(g->suivant, s);
 }
@@ -60,7 +117,7 @@ Nat gNombreSommets(Graphe g)
 static Nat gNombreAretesAux(Graphe g, Nat n)
 {
     if (gEstVide(g)) return n;
-    else return gNombreAretesAux(g->suivant, n + ltaille(g->sommet));
+    else return gNombreAretesAux(g->suivant, n + ltaille(g->listeadjacence));
 }
 
 Nat gNombreAretes(Graphe g)

fa20fbf0d80a85722674aa6036e731874d28ff48
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2013-04-03 23:49:14 +0200

    doc aretes

--- include/arete.h
+++ include/arete.h
@@ -1,6 +1,6 @@
 /**
  * \file arete.h
- * \brief Arete (header)
+ * \brief Arêtes (header)
  * \author Harenome RAZANAJATO
  */
 #ifndef __ARETE_H
@@ -18,9 +18,9 @@
  */
 struct Arete
 {
-    Sommet * a;   /**<- Extrémité a de l'arête. */
-    Sommet * b;   /**<- Extrémité b de l'arête. */
-    Ent poids;  /**<- Poids de l'arête. */
+    Sommet * a;   /**<- Extrémité a de l'Arete. */
+    Sommet * b;   /**<- Extrémité b de l'Arete. */
+    Ent poids;    /**<- Poids de l'Arete. */
 };
 
 /**
@@ -31,83 +31,93 @@ typedef struct Arete Arete;
 
 
 /**
- * \brief Nouvelle arête.
+ * \brief Nouvelle Arete.
  * \relatesalso Arete
- * \param a Extrémité a de l'arête.
- * \param b Extrémité b de l'arête.
- * \param poids Poids de l'arête.
- * \return (pointeur vers) Nouvelle arête (a, b) de poids poids.
+ * \param a Extrémité a de l'Arete.
+ * \param b Extrémité b de l'Arete.
+ * \param poids Poids de l'Arete.
+ * \return (pointeur vers) Nouvelle Arete (\a a, \a b) de poids \a poids.
  */
 Arete * aNouv(const Sommet * a, const Sommet * b, Ent poids);
 
 /**
- * \brief Extrémité a de l'arête.
+ * \brief Extrémité a de l'Arete.
  * \relatesalso Arete
  * \param a Arete.
- * \return Sommet à l'extrémité a de l'arête.
+ * \return ::Sommet à l'extrémité \a a de l'Arete \a a.
  */
 Sommet * aA(const Arete * a);
 
 /**
- * \brief Extrémité b de l'arête.
+ * \brief Extrémité b de l'Arete.
  * \relatesalso Arete
  * \param a Arete.
- * \return Sommet b l'extrémité a de l'arête.
+ * \return ::Sommet à l'extrémité \a b de l'Arete \a a.
  */
 Sommet * aB(const Arete * a);
 
 /**
- * \brief Poids de l'arête.
+ * \brief Poids de l'Arete.
  * \relatesalso Arete
  * \param a Arete.
- * \return Poids de l'arête.
+ * \return Poids de l'Arete \a a.
  */
 Ent aPoids(const Arete * a);
 
 /**
- * \brief Test d'égalité de deux arêtes, sans se préoccuper du poids.
+ * \brief Test d'égalité de deux Aretes, sans se préoccuper du poids.
  * \relatesalso Arete
  * \param a Arete.
  * \param s Extrémité.
  * \param t Autre extrémité.
- * \return VRAI si les extrémités de a correspondent à s et t, FAUX sinon.
+ * \return #VRAI si les extrémités de l'Arete \a a correspondent à \a s et \a t, FAUX sinon.
+ *
+ * Le poids de l'Arete n'a pas d'importance. Compare les extrémités de \a a à \a s et \a t.
  */
 Bool aAreteEgaleS(const Arete * a, const Sommet * s, const Sommet * t);
 
 /**
- * \brief Test d'égalité de deux arêtes, sans se préoccuper du poids.
+ * \brief Test d'égalité de deux Aretes, sans se préoccuper du poids.
  * \relatesalso Arete
  * \param a Arete.
  * \param b Arete.
  * \return VRAI si les extrémités de a et b sont identiques, FAUX sinon.
+ *
+ * Les poids des Arete n'importent pas, on ne compare que les extrémités des Arete \a a et \a b. L'ordre des extrémités n'importe pas non plus.
  */
 Bool aAreteEgaleS2(const Arete * a, const Arete * b);
 
 /**
- * \brief Test d'égalité de deux arêtes, sensible au poids.
+ * \brief Test d'égalité de deux Aretes, sensible au poids.
  * \relatesalso Arete
  * \param a Arete.
  * \param s Extrémité.
  * \param t Autre extrémité.
  * \param poids Poids.
  * \return VRAI si les extrémités de a correspondent à s et t et si le poids est le même, FAUX sinon.
+ *
+ * Compare les extrémités de l'Arete \a a à \a s et \a t et le poids de l'Arete à \a poids.
  */
 Bool aAreteEgaleP(const Arete * a, const Sommet * s, const Sommet * t, Ent poids);
 
 /**
- * \brief Test d'égalité de deux arêtes, sans se préoccuper du poids.
+ * \brief Test d'égalité de deux Aretes, sans se préoccuper du poids.
  * \relatesalso Arete
  * \param a Arete.
  * \param b Arete.
  * \return VRAI si les extrémités et les poids de a et b sont identiques, FAUX sinon.
+ *
+ * Compare les extrémités et les poids des Arete \a a et \a b.
  */
 Bool aAreteEgaleP2(const Arete * a, const Arete * b);
 
 /**
- * \brief Libérer une arête.
+ * \brief Libérer une Arete.
  * \relatesalso Arete
  * \param a Arete à libérer.
  * \return NULL
+ *
+ * Libère la mémoire occupée par l'Arete a : libère aussi la mémoire occupée par les ::Sommet extrémités de \a a.
  */
 Arete * aLiberer(Arete * a);
 

901b358fcaa299800f8ff7cf1159a39b329757f5
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2013-04-17 12:01:21 +0200

    gestion erreurs, doc

--- include/generation.h
+++ include/generation.h
@@ -14,6 +14,17 @@
 #include "listearete.h"
 #include "arete.h"
 
+/**
+ * \brief Générer un fichier latex représentant le Graphe \a g à l'aide de TikZ.
+ * \relatesalso Graphe
+ * \param g Graphe à représenter.
+ * \param destination Chemin vers le fichier de destination.
+ * \return \a 0 en cas de succès, une valeur négative en cas d'erreur.
+ * 
+ * Erreurs :
+ * - \a -1 : Le fichier de destination n'a pas pu être ouvert.
+ * - \a -2 : Il y a eu une erreur lors de l'écriture.
+ */
 int gGenererLatex(const Graphe * g, const char * destination);
 
 #endif /* __GENERATION_H */

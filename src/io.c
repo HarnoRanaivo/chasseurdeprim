f01b3a0a5798c2cdcfb3890b981cc6c8347ce803
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2013-04-27 22:51:45 +0200

    retrait assert

--- src/io.c
+++ src/io.c
@@ -14,38 +14,47 @@
 void sauvegarder_graphe (const Graphe * g, const char* destination){
 	FILE * pfile = NULL;
 	pfile = fopen(destination, "w"); // crée le fichier destination s'il n'existe pas sinon le réécrit
-	assert (pfile != NULL);
 
-    for (const ListeArete * l = gAretes(g); !larEstVide(l); l = larSuivante(l))
+	if (pfile != NULL)
     {
-        const Arete * const a = larAreteTete(l);
-        fprintf(pfile, "%s\t%d\t%s\n", aA(a), aPoids(a), aB(a));
+        for (const ListeArete * l = gAretes(g); !larEstVide(l); l = larSuivante(l))
+        {
+            const Arete * const a = larAreteTete(l);
+            fprintf(pfile, "%s\t%d\t%s\n", aA(a), aPoids(a), aB(a));
+        }
+
+        fclose (pfile); // ferme le fichier
     }
 
-	fclose (pfile); // ferme le fichier
 	return;
 }
 
 Graphe * charger_graphe (const char* source){
 	FILE * pfile = NULL;
 	pfile = fopen(source, "r"); // lit le fichier source
-	assert (pfile != NULL);
+
 	Graphe * g = gNouv();
 	
-	int retourFs;
-	char buffer1[100]; // sommet de départ de 100 caractères maximum
-	Ent poids;
-	char buffer2[100]; // sommet d'arrivée de 100 caractères maximum
-	
-	while ((retourFs = fscanf(pfile,"%99s\t%d\t%99s",buffer1,&poids,buffer2)) != EOF){ // vérifie si la ligne n'est pas la fin du fichier
-		if (retourFs == 3)
-			g = gAjouterArete(g,buffer1,buffer2,poids); // ajoute l'arc à partir des valeurs de la ligne
-		else // cas où la lecture a raté
-			fprintf(stderr,"Le fichier a mal été écrit, veuillez s'il vous plait respecter le code d'écriture qui :\nSommet d'entrée \\t Poids \\t Sommet d'arrivée\\n\n");
-			// à méditer si l'on quitte le prog ou pas
-	}
-	printf("Fin de lecture.\n\n");
-	
-	fclose (pfile);
+    if (pfile != NULL)
+    {
+        int retourFs;
+        char buffer1[100]; // sommet de départ de 100 caractères maximum
+        Ent poids;
+        char buffer2[100]; // sommet d'arrivée de 100 caractères maximum
+        Bool erreurs = FAUX;
+        
+        while ((retourFs = fscanf(pfile,"%99s\t%d\t%99s",buffer1,&poids,buffer2)) != EOF){ // vérifie si la ligne n'est pas la fin du fichier
+            if (retourFs == 3)
+                g = gAjouterArete(g,buffer1,buffer2,poids); // ajoute l'arc à partir des valeurs de la ligne
+            else // cas où la lecture a raté
+                erreurs = VRAI;
+        }
+
+        if (erreurs)
+            fprintf(stderr, "Le fichier semble contenir des erreurs.\n");
+        
+        fclose (pfile);
+    }
+
 	return g;
 }
